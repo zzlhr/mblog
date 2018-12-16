8064616ba2753e70fe5c884dad09f90280435a8d
zzlhr <2388399752@qq.com>
2018-12-16 10:46:29 +0800

    init commit

---
+++ src/main/java/com/lhrsite/mblog/MblogApplication.java
@@ -0,0 +1,14 @@
+package com.lhrsite.mblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MblogApplication.class, args);
+    }
+
+}
+
